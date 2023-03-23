4991d241a9c801dbe0b53f4d23e06f0ccc815792
Antonis <antonis.papadakis@protonmail.com>
2023-03-23 13:03:04 +0200

    Implement bot using a class

--- discord_bot.py
+++ discord_bot.py
@@ -1,94 +1,99 @@
 import asyncio
 import discord
 from discord.ext import commands
-# from datetime import datetime
 from twitter_functions import get_user, get_new_friends
 from db_functions import add_user, remove_user, check_for_user, read_channel_id, write_channel_id, get_tracked_users, update_friends_number, delete_channel_id
 from config import read_config
 
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+class TwitterTrackerBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.tracked_channel_id = None
+        self.stop_event = asyncio.Event()
+
+    async def on_ready(self):
+        print("Bot is online")
+        try:
+            synced = await self.tree.sync()
+            print(f"Synced {len(synced)} command(s)")
+        except Exception as e:
+            print(e)
+
+        self.tracked_channel_id = read_channel_id()
+        if self.tracked_channel_id is not None:
+            self.loop.create_task(self.track_users())
+
+    async def track_users(self):
+        while bot.tracked_channel_id is not None:
+            tracked_users = get_tracked_users()
+            if tracked_users:   # check if the database is not empty
+                for user in tracked_users:
+                    (username, friends_num) = user
+                    (new_friends, diff) = get_new_friends(username, friends_num)
+                    if new_friends is not None:
+                        update_friends_number(username, diff)
+
+                        for friend in new_friends:
+                            profile = friend.screen_name
+                            url = f"https://www.twitter.com/{profile}"
+                            follower_num = friend.followers_count
+                            pfp_url = friend.profile_image_url
+                            date_created = friend.created_at.date()
+
+                            embed = discord.Embed(color=discord.Color.blue())
+
+                            embed.add_field(
+                                name="",
+                                value=f"**[{profile}]({url})**",
+                                inline=False
+                            )
+
+                            embed.add_field(
+                                name="**Was followed by:**",
+                                value=username,
+                                inline=False
+                            )
+
+                            embed.add_field(
+                                name="**Profile Stats:**",
+                                value=(
+                                    f"Followers: {follower_num}\n"
+                                    f"Account Created: {date_created}"
+                                ),
+                                inline=False
+                            )
+
+                            embed.set_thumbnail(url=pfp_url)
+
+                            channel = bot.get_channel(bot.tracked_channel_id)
+                            await channel.send(embed=embed)
+                    elif diff < 0:
+                        update_friends_number(username, diff)
+
+            await asyncio.sleep(10)
+
+            if self.stop_event.is_set():
+                self.stop_event.clear()  # Reset the stop_event for future use
+                break
 
 
-@bot.event
-async def on_ready():
-    print("Bot is online")
-    try:
-        synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} command(s)")
-    except Exception as e:
-        print(e)
-
-    # Read the stored channel_id when the bot comes online
-    bot.tracked_channel_id = read_channel_id()
-    if bot.tracked_channel_id is not None:
-        bot.loop.create_task(track_users())
-
-
-async def track_users():
-    while bot.tracked_channel_id is not None:
-        tracked_users = get_tracked_users()
-        if tracked_users:   # check if the database is not empty
-            for user in tracked_users:
-                (username, friends_num) = user
-                (new_friends, diff) = get_new_friends(username, friends_num)
-                if new_friends is not None:
-                    update_friends_number(username, diff)
-
-                    for friend in new_friends:
-                        profile = friend.screen_name
-                        url = f"https://www.twitter.com/{profile}"
-                        follower_num = friend.followers_count
-                        pfp_url = friend.profile_image_url
-                        date_created = friend.created_at.date()
-
-                        embed = discord.Embed(color=discord.Color.blue())
-
-                        embed.add_field(
-                            name="",
-                            value=f"**[{profile}]({url})**",
-                            inline=False
-                        )
-
-                        embed.add_field(
-                            name="**Was followed by:**",
-                            value=username,
-                            inline=False
-                        )
-
-                        embed.add_field(
-                            name="**Profile Stats:**",
-                            value=(
-                                f"Followers: {follower_num}\n"
-                                f"Account Created: {date_created}"
-                            ),
-                            inline=False
-                        )
-
-                        embed.set_thumbnail(url=pfp_url)
-
-                        channel = bot.get_channel(bot.tracked_channel_id)
-                        await channel.send(embed=embed)
-                elif diff < 0:
-                    update_friends_number(username, diff)
-
-        await asyncio.sleep(10)
+bot = TwitterTrackerBot(command_prefix='!', intents=discord.Intents.all())
 
 
 @bot.tree.command(name="start", description="Initialize the bot")
 async def start(interaction: discord.Interaction):
     bot.tracked_channel_id = interaction.channel_id
     user = str(interaction.user)
-    # Store the channel_id and the discord user when the /start command is called
     write_channel_id(bot.tracked_channel_id, user)
     await interaction.response.send_message("Bot initiated", ephemeral=True)
-    bot.loop.create_task(track_users())  # Start the track_users task here
+    bot.loop.create_task(bot.track_users())
 
 
 @bot.tree.command(name="stop", description="Stop the bot")
 async def stop(interaction: discord.Interaction):
     bot.tracked_channel_id = None
-    # Deletethe channel_id from the database
+    bot.stop_event.set()  # Signal the track_users loop to stop
     delete_channel_id(interaction.channel_id)
     await interaction.response.send_message("Bot stopped", ephemeral=True)
 
@@ -100,18 +105,16 @@ async def add(interaction: discord.Interaction, username: str):
         await interaction.response.send_message(f"User {username} doesn't exist", ephemeral=True)
     else:
         await interaction.response.send_message(f"User {username} added", ephemeral=True)
-        add_user(username)    # add user to the database
+        add_user(username)
 
 
 @bot.tree.command(name="remove", description="Remove Twitter user from tracker")
 async def remove(interaction: discord.Interaction, username: str):
-    # user = get_user(username)
     if check_for_user(username) is None:
         await interaction.response.send_message(f"User {username} not in the database", ephemeral=True)
     else:
         await interaction.response.send_message(f"User {username} removed", ephemeral=True)
         remove_user(username)    # remove user to the database
-
 
 config_values = read_config()
 discord_api_key = config_values['discord']['api_key']
